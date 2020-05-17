50453f6ad5c16fda9f91dde89b64ebdca2e520fb
Your Name <you@example.com>
2020-05-18 00:28:42 +0900

    add other

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const tweet = require('./lib/tweet');
 const retweet = require('./lib/retweet');
-const search = require('./lib/search');
+const { search, generateQuery } = require('./lib/search');
 const { timeline, outputTweets, recentTweet } = require('./lib/timeline');
+const DateUtil = require('./lib/date_util');
+const dateUtil = new DateUtil();
 
-const flg = true;
+
+const flg = false;
 if(flg) {
   const target_screen_name = 'tocho_koho';
   timeline(target_screen_name, function(err, tweets) {
@@ -18,13 +21,22 @@ if(flg) {
   });
 } else {
   const params = { 
-    q: 'コロナ until:2020-04-30_07:00:00_JST',
-    result_type: 'recent',
+    q: generateQuery({
+      keyword: '東京最終',
+      // since: true,
+      // until: true,
+      // filter: 'native_video',
+      // outFilter: 'retweets',
+      // from: 'yutaro0758',
+      // mention: 'tocho_koho'
+    }),
+    result_type: 'popular',
     count: 5,
     // until: '2020-04-22',
     lang: 'ja',
     locale: 'ja',
   };
+  console.log('query: ' + params.q);
   search(params, function(err, tweets) {
     if(err) return console.log(err);
     outputTweets(tweets, function(err) {
