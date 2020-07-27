61894177cbc499723ccb6e8585275e35bd15aead
zaburoh <you.the.blow@gmail.com>
2020-07-28 00:28:15 +0900

    fix gpio.

--- index_gpio.js
+++ index_gpio.js
@@ -1,56 +1,52 @@
 const tweet = require('./lib/twitter/tweet');
-const ReadSwitch = require('./lib/gpio/read_switch');
-const DateUtil = require('./lib/util/date_util');
 const raspberrypiStatus = require('./lib/util/raspberrypi_status');
+const moment = require('moment');
+const GPIO = require('./lib/gpio/gpio');
+const gpio = new GPIO();
 
-let readSwitch = new ReadSwitch();
-let pin = process.argv[2];
-let direction = 'in';
+const Trig = 4;
+const Twet17 = 17;
+let message = '';
 
-readSwitch.exportPin(pin)
-  .on('error', err => {
-    console.log('[exportError]' + err);
-    readSwitch.unexportPin(pin);
-  })
-  .on('success', pin => {
-    console.log('[exportPin]' + pin)
-    readSwitch.setDirectionPin(pin, direction)
-      .on('error', err => {
-        console.log('[directionError]' + err);
-        readSwitch.unexportPin(pin);
-      })
-      .on('success', set => {
-        console.log('[directionPin]' + set.pin + ':' + set.direction);
+async function led(count, interval) {
+  for(let i = 0; i < count; i++) {
+    await gpio.sleep(interval);
+    await gpio.output(Trig, gpio.OUTPUT.HIGH);
+    await gpio.sleep(interval);
+    await gpio.output(Trig, gpio.OUTPUT.LOW);  
+  }
+}
 
-        function iterate() {
-          readSwitch.readPin(pin, (err, state) => {
-            if(err) {
-              readSwitch.unexportPin(pin);
-              return console.log('[READ ERROR]' + err);
-            }
-            if(state) {
-              console.log('[pushed]' + pin);
-              let message = new DateUtil().formatedDate() + '\n\n';
-              if(pin === '17') {
-                raspberrypiStatus.measure_temp((err, stdout) => {
-                  if(err) return console.log('[ERROR]', err);
-                  message +=  stdout + '\n\n';
-                  tweet(message, function() {
-                    iterate();
-                  });
-                })
-              } else {
-                message += pin + '番のスイッチが押されました。' + '\n\n';
-                tweet(message, function() {
-                  iterate();
-                });  
-              }
-            } else {
-              iterate();
-            }
-          });
-        }
-        iterate();
-      });
+(async function() {
+
+  process.once('SIGINT', async () => {
+    try {
+      await gpio.unexportPin(Trig);
+      await gpio.unexportPin(Twet17);
+    } catch(err) {
+      console.log(err);
+    }
+    process.exit();
   });
 
+  try {
+    await gpio.setup(Trig, gpio.DIRECTION.OUT);
+    await gpio.setup(Twet17, gpio.DIRECTION.IN);
+
+    await gpio.input(Twet17, function(err, pin) {
+      led(3, 100);
+      message = moment().format('YYYY-MM-DD HH:mm:ss') + '\n\n' + pin + '番のスイッチが押されました。' + '\n\n';
+      raspberrypiStatus.measure_temp(function(err, stdout) {
+        message += 'RaspberryPiのCPU温度は ' + stdout + ' です。';
+        tweet(message, function() {
+          console.log('pushed: ' + pin);
+        });    
+      })
+    });
+
+  } catch(err) {
+    console.log(err);
+    await gpio.unexportPin(Trig);
+    await gpio.unexportPin(Twet17);
+  }
+})();
