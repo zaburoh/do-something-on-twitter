a9c4a91c05d23f805f5b791b9ce8d13544131877
zaburoh <you.the.blow@gmail.com>
2021-02-07 17:05:27 +0900

    add: twtterで”ISBN"を検索した結果からopenBDで書籍情報を取得してツイートする

--- periodic_execution.js
+++ periodic_execution.js
@@ -1,7 +1,12 @@
 const tweet = require('./lib/twitter/tweet');
 const woeid = require('./woeid');
 const { place, getRandomTrend } = require('./lib/twitter/trends');
+const { search, generateQuery } = require('./lib/twitter/search');
 const moment = require('moment');
+const MeCab = require('mecab-async');
+const mecab = new MeCab();
+const Books = require('./lib/books/books');
+const books = new Books();
 
 module.exports = {
   params: {
@@ -15,6 +20,47 @@ module.exports = {
         tweet(moment().format('YYYY-MM-DD HH:mm:ss') + '\n\n' + 'なんで' + trend.name.replace(/#/, '') + 'がトレンドに入っているんだろう...？\n\n');
       })
     });
+  },
+  isbnTweet: function() {
+    const params = { 
+      q: generateQuery({
+        keyword: '"ISBN"',
+        outFilter: 'retweets',
+      }),
+      result_type: 'recent',
+      count: 5,
+      lang: 'ja',
+      locale: 'ja',
+    };
+    search(params, function(err, tweets) {
+      if(err) return console.log(err);
+      tweets.forEach((t) => {
+        mecab.parse(t.text, (err, results) => {
+          if(err) return console.log(err);
+          results.forEach(async (r) => {
+            if(r[2] == '数') {
+              let isbn = r[0];
+              if(isbn.length == 13 || isbn.length == 10) { 
+                try {
+                  let result = await books.search(isbn);
+                  result.forEach(r => {
+                    let bookInfo = r.summary;
+                    tweet(
+                      moment().format('YYYY-MM-DD HH:mm:ss') + '\n\n' 
+                      // + bookInfo.isbn + '\n' 
+                      + bookInfo.title + '\n' 
+                      + bookInfo.author + '\n'
+                      // + bookInfo.cover
+                    );
+                  });
+                } catch(e) {
+                  return console.log(e);
+                }
+              }
+            }
+          });
+        });
+      });
+    });
   }
-
 }
--- schedule-cron.js
+++ schedule-cron.js
@@ -4,3 +4,7 @@ let app = require('./periodic_execution');
 new CronJob('00 15 * * * *', () => {
   app.trendTweet();
 }, null, true);
+
+new CronJob('00 45 * * * *', () => {
+  app.isbnTweet();
+}, null, true);
